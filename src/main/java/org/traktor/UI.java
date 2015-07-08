aa6f393ff81f6eaa33588c9c64ac32af15e39ec9
bonapeti <bonapeti@gmail.com>
2015-07-08 06:21:20 +0200

    Add new UI class

---
+++ src/main/java/org/traktor/UI.java
@@ -0,0 +1,11 @@
+package org.traktor;
+
+import org.springframework.boot.SpringApplication;
+
+public class UI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UI.class, args);
+	}
+
+}
